7053a1b4bb1749730baed53050709b369214a194
wxd123456789 <1042873383@qq.com>
2020-09-01 01:45:01 +0800

    update ignore

--- src/main/java/com/first_spring_demo/common/utils/UUIDHelper.java
+++
@@ -1,9 +0,0 @@
-package com.first_spring_demo.common.utils;
-
-import java.util.UUID;
-
-public class UUIDHelper {
-    public static String uuid(){
-        return UUID.randomUUID().toString().replaceAll("-", "");
-    }
-}
